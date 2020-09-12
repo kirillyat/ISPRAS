d9e8a37cd79bf80217bcb9e004d2fea01dcba3f9
Kirill <kirillyat@gmail.com>
2020-09-12 19:38:16 +0300

    Add files via upload

---
+++ QuadraticEquation/QuadraticEquation.cpp
@@ -0,0 +1,102 @@
+#include <cmath>
+#include <iostream>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include "QuadraticEquation.hpp"
+
+
+int QuadraticEquationSolver(double a, double b, double c,
+                            double *root1, double *root2)
+{
+    double D;
+
+    if (isZero(a)) {
+        if (isZero(b)) {
+            return (isZero(c)) ? INF: 0;
+        } else { /* if (b != 0) */
+            *root1 = -c/b;
+            return 1;
+        }
+    } else { /* if (a != 0) */
+        D = b*b - 4*a*c;
+        if (D < 0) {
+            return 0;
+        } else if (isZero(D)) {
+            *root1 = -b/(2*a);
+            return 1;
+        } else { /* if(D != 0) */
+            *root1 = (-b + sqrt(D))/(2*a);
+            *root2 = (-b - sqrt(D))/(2*a);
+            return 2;
+        }
+    }
+}
+
+
+
+void PrintEquation(double a, double b, double c)
+{
+    bool af = notZero(a),
+         bf = notZero(b),
+         cf = notZero(c);
+
+    if (af) {
+        std::cout << a << "x^2 ";
+        if (bf || cf)
+            std::cout << "+ ";
+    }
+
+    if (bf) {
+        std::cout << b << "x ";
+        if (cf)
+            std::cout << "+ ";
+    }
+
+    if (cf) {
+        std::cout << c;
+    }
+
+    if (!(af || bf || cf))
+    	std::cout << "0";
+
+    std::cout << " = 0";
+}
+
+
+
+
+void PrintAnalysis(int AmountOfRoots,
+                   const double *root1,
+                   const double *root2)
+{
+
+
+
+    switch (AmountOfRoots) {
+        case INF:
+            std::cout << "Infinity amount of roots" << std::endl;
+        break;
+        case 0:
+            std::cout << "No roots" << std::endl;
+        break;
+        case 1:
+            std::cout << "Only one root: " << *root1 << std::endl;
+        break;
+        case 2:
+            std::cout << "Two roots: " << *root1 << " and " << *root2 << std::endl;
+        break;
+    }
+}
+
+
+bool  isZero(double d)
+{
+    return std::fabs(d) < EPS;
+}
+
+
+bool notZero(double d)
+{
+    return !isZero(d);
+}
---
+++ QuadraticEquation/README.md
@@ -0,0 +1,23 @@
+<a href="https://www.ispras.ru" target="_blank"><img src="http://talisman.ispras.ru/wp-content/uploads/2019/01/logo_RU.jpg" alt="drawing" width="200"/></a>
+
+# Quadratic Equation Solver
+
+### Запуск: 
+##### make  и  ./QE
+
+### Задача:
+- Написать программу, которая будет решать квадратное уравнение
+- Добавить документацию (Doxygen)
+- Написать unittest`ы (нет)
+- Написать Make файл
+
+
+
+### Ссылки на полезные ресурсы:
+- <a href="https://habr.com/ru/post/141080" target="_blank">Assert</a>
+- <a href="https://habr.com/ru/post/252101" target="_blank">Doxygen</a>
+- <a href="https://github.com/sandino/Markdown-Cheatsheet" target="_blank">Markdown</a>
+- <a href="https://habr.com/ru/post/155201" target="_blank">Makefile</a>
+- <a href="https://habr.com/ru/post/169381" target="_blank">Unittest</a>
+
+##### Автор: Яценко Кирилл Сергеевич (kirillyat)
