5e99e2f8ced796d5072ac12003d0d77006977fbd
kirillyat <kirillyat@gmail.com>
2020-09-21 17:38:54 +0300

    string compare functions

--- Onegin/Onegin.cpp
+++ Onegin/Onegin.cpp
@@ -1,1 +1,44 @@
+#include <string>
+#include <cctype>
 
+int FrontStringCompare(char* str1, char* str2)
+{
+    int index1 = 0,
+        index2 = 0;
+
+    while ((str1[i] != 0) || (str2[i] != 0)) {
+        if (!isalpha(str1[index1])) {
+            index1++;
+            continue;
+        }
+        if (!isalpha(str1[index2])) {
+            index2++;
+            continue;
+        }
+        if (str1[index1] - str2[index2] != 0) {
+            return str1[index1] - str2[index2];
+        }
+    }
+    return str1[index1] - str2[index2];
+}
+
+bool  BackStringCompare(char* str1, char* str2)
+{
+    int index1 = length(str1),
+        index2 = length(str2);
+
+    while ((index1 > 0) || (index1 > 0)) {
+        if (!isalpha(str1[index1])) {
+            index1--;
+            continue;
+        }
+        if (!isalpha(str1[index2])) {
+            index2--;
+            continue;
+        }
+        if (str1[index1] - str2[index2] != 0) {
+            return str1[index1] - str2[index2];
+        }
+    }
+    return str1[index1] - str2[index2];
+}
---
+++ Onegin/text.cpp
@@ -0,0 +1,12 @@
+#include <cctype>
+
+
+bool FrontStringCompare(char* str1, char* str2)
+{
+
+}
+
+bool  BackStringCompare(char* str1, char* str2)
+{
+
+}
